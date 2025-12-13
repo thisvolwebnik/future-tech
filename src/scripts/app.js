406052706baf978fbfb08aab00625b32b2bb242c
Nikita(thisvolwebnik) <thisvolwebnik@yandex.ru>
2025-12-13 21:07:41 +0300

    feat: add video player functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
 import "../styles/style.scss";
 import Header from "./Header";
 import TabsCollection from "./Scripts";
+import VideoPlayerCollection from "./VideoPlayer";
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
