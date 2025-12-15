6f2f5dffce260ce2d2831740091d39927bdc2462
Nikita(thisvolwebnik) <thisvolwebnik@yandex.ru>
2025-12-15 18:15:38 +0300

    feat: add new blog page with HTML structure

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        news: resolve(__dirname, "src/pages/news.html"),
       },
     },
   },
